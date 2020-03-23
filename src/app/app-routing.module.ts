461cc00d95130be073c5067c4dda72b9122a4e9e
oYunYo <victorichard38@gmail.com>
2020-03-23 23:11:07 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  // {path: 'planets', component: PlanetsComponent},
+  // {path: 'planets/add', component: AddPlanetComponent},
+  // {path: 'planets/:id', component: PlanetDetailComponent},
+  // {path: 'planets/edit/:id', component: EditPlanetComponent},
+  // {path: 'vehicles', component: VehiculsComponent},
+  // {path: 'vehicles/add', component: AddVehicleComponent},
+  // {path: 'vehicles/edit/:id', component: EditVehicleComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './components/menu/menu.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CollapseModule } from 'ngx-bootstrap';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    CollapseModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
